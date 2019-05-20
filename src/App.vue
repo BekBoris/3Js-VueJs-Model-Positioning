eea09328fa462f923dfdc80674f2702710ebd412
Boris <bekboris@gmail.com>
2019-05-20 23:00:44 +0400

    3JS VueJS Model Positioning

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,175 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mainCont">
+    <div id="container"></div>
+
+    <div>
+        <p>Position</p>
+        <input type="number" name="position" v-model="position.x" @change="inputChange" />
+        <input type="number" name="position" v-model="position.y" @change="inputChange" />
+        <input type="number" name="position" v-model="position.z" @change="inputChange" />
+    </div>
+
+    <div>
+        <p>Rotation</p>
+        <input type="number" name="rotation" v-model="rotation.x" @change="inputChange" />
+        <input type="number" name="rotation" v-model="rotation.y" @change="inputChange" />
+        <input type="number" name="rotation" v-model="rotation.z" @change="inputChange" />
+    </div>
+
+    <div>
+        <p>Scale</p>
+        <input type="number" name="scale" v-model="scale.x" @change="inputChange" />
+        <input type="number" name="scale" v-model="scale.y" @change="inputChange" />
+        <input type="number" name="scale" v-model="scale.z" @change="inputChange" />
+    </div>
+
+      <div>
+      <p>Position Rotation Scaled Selector</p>
+      <select @change="posRotScale" v-model="selectTool">
+        <option>position</option>
+        <option>rotation</option>
+        <option>scale</option>
+      </select>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import  TransformControls  from './TransformControls.js';
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'THREETest',
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      gltf: null,
+      orbit: null,
+      control: null,
+      selectTool: "position",
+      position: { x: 0, y: 0, z: 0 },
+      rotation: { x: 0, y: 0, z: 0 },
+      scale: { x: 3, y: 3, z: 3 },
+    }
+  },
+  methods: {
+    init: function() {
+        let container = document.getElementById('container');
+        // SET AND ADD CAMERA
+        this.camera = new THREE.PerspectiveCamera(75, container.clientWidth/container.clientHeight, 0.1, 1000);
+        this.camera.position.z = 1;
+        // ADD SCENE
+        this.scene = new THREE.Scene();
+        // ADD LIGHT TO SCENE
+        this.light = new THREE.AmbientLight(0xffffff, 5.3);
+        this.scene.add(this.light);
+        // RENDER IN PAGE
+        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.renderer.setClearColor('#dbdbdb');
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        this.renderer.gammaFactor = 1.5;
+        this.renderer.gammaOutput = true;
+        container.appendChild(this.renderer.domElement);
+
+        // SET GRID HELPER
+        const size = 10;
+        const divisions = 25;
+        this.gridHelper = new THREE.GridHelper( size, divisions );
+        this.scene.add( this.gridHelper );
+
+        // SET ORBIT CONTROLS
+        this.orbit = new OrbitControls( this.camera, this.renderer.domElement );
+        this.orbit.addEventListener("change", () => this.renderer.render(this.scene, this.camera));
+        this.camera.position.set( 0, 2, 2 );
+        this.orbit.update();
+
+        // SET MODEL CONTROL
+        this.control = new TransformControls( this.camera, this.renderer.domElement );
+        this.control.setSize(1);
+        this.control.addEventListener( 'change', () => { this.renderer.render(this.scene, this.camera); });
+
+        // EVENT LISTNER TO ENABLE ORBIT CAMERA AND CHANGE VALUE OF INPUTS
+        this.control.addEventListener( 'dragging-changed', ( event ) => {
+          this.orbit.enabled = ! event.value});
+          const gltf = this.gltf[this.selectTool];
+          this[this.selectTool] = {x: gltf.x, y: gltf.y, z: gltf.z}
+
+
+        // ADD GLTFLOADER
+        this.loader = new GLTFLoader();
+
+        this.loader.load("./meat.glb", gltf => {
+          this.gltf = gltf.scene;
+          this.scene.add(gltf.scene);
+
+          // ADD CONTROLS TO MODEL
+          this.control.attach( gltf.scene );
+    			this.scene.add( this.control );
+
+          gltf.scene.scale.set(3, 3, 3)
+          this.orbit.update();
+
+          this.renderer.render(this.scene, this.camera);
+        }, undefined,
+        error => {
+          console.log(error);
+        });
+
+        this.renderer.render(this.scene, this.camera);
+
+
+    },
+
+    // CONTROLS SET MODE CHANGE
+    posRotScale: function(e) {
+      switch (e.target.options.selectedIndex) {
+
+        case 0:
+          this.control.setMode( "translate" );
+          break;
+
+        case 1:
+          this.control.setMode( "rotate" );
+          break;
+
+        case 2:
+          this.control.setMode( "scale" );
+          break;
+
+    }
+  },
+
+    // INPUT CHAGE 
+    inputChange: function(e) {
+      const selectOpt = e.target.name
+      const setNum = this[selectOpt];
+      this.gltf[selectOpt].set(setNum.x, setNum.y, setNum.z)
+      this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted() {
+     this.init();
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#container {
+  height: 500px;
+  width: 500px;
+}
+*{
+  margin: 0px;
+  padding: 0px;
+}
+.mainCont {
+  width: 500px;
+  margin: auto;
 }
 </style>
