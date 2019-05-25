c6266ba2075a80ee91a8108b9a29495502c180c6
Boris <bekboris@gmail.com>
2019-05-25 16:38:06 +0400

    3JS VueJS Model Positioning

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="mainCont">
-    <div id="container"></div>
+  <div class="mainContainer">
+    <div ref="container" id="container"></div>
 
     <div>
         <p>Position</p>
-        <input type="number" name="position" v-model="position.x" @change="inputChange" />
+        <input ref="text" type="number" name="position" v-model="position.x" @change="inputChange" />
         <input type="number" name="position" v-model="position.y" @change="inputChange" />
         <input type="number" name="position" v-model="position.z" @change="inputChange" />
     </div>
@@ -25,7 +25,7 @@
 
       <div>
       <p>Position Rotation Scaled Selector</p>
-      <select @change="posRotScale" v-model="selectTool">
+      <select  @change="positionRotationScale" v-model="selectTool">
         <option>position</option>
         <option>rotation</option>
         <option>scale</option>
@@ -58,79 +58,68 @@ export default {
       scale: { x: 3, y: 3, z: 3 },
     }
   },
+  mounted() {
+    let container = this.$refs.container;
+    // SET AND ADD CAMERA
+    this.camera = new THREE.PerspectiveCamera(75, container.clientWidth/container.clientHeight, 0.1, 1000);
+    this.camera.position.z = 1;
+    // ADD SCENE
+    this.scene = new THREE.Scene();
+    // ADD LIGHT TO SCENE
+    this.light = new THREE.AmbientLight(0xffffff, 5.3);
+    this.scene.add(this.light);
+    // RENDER IN PAGE
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setClearColor('#dbdbdb');
+    this.renderer.setSize(container.clientWidth, container.clientHeight);
+    this.renderer.gammaFactor = 1.5;
+    this.renderer.gammaOutput = true;
+    container.appendChild(this.renderer.domElement);
+    // SET GRID HELPER
+    const size = 10;
+    const divisions = 25;
+    this.gridHelper = new THREE.GridHelper( size, divisions );
+    this.scene.add( this.gridHelper );
+    // SET ORBIT CONTROLS
+    this.orbit = new OrbitControls( this.camera, this.renderer.domElement );
+    this.camera.position.set( 0, 2, 2 );
+    this.orbit.update();
+    this.orbit.addEventListener("change", () => this.renderer.render(this.scene, this.camera));
+    // SET MODEL CONTROL
+    this.control = new TransformControls( this.camera, this.renderer.domElement );
+    this.control.setSize(1);
+    this.control.addEventListener( 'change', () => { this.renderer.render(this.scene, this.camera); });
+    // EVENT LISTNER TO ENABLE ORBIT CAMERA AND CHANGE VALUE OF INPUTS
+    this.control.addEventListener( 'dragging-changed', ( event ) => {
+      const gltf = this.gltf[this.selectTool];
+      this[this.selectTool] = {x: gltf.x, y: gltf.y, z: gltf.z}
+      this.orbit.enabled = ! event.value
+    });
+    // ADD GLTFLOADER
+    this.loader = new GLTFLoader();
+
+    this.loader.load("./meat.glb", gltf => {
+      this.gltf = gltf.scene;
+      this.scene.add(gltf.scene);
+
+      // ADD CONTROLS TO MODEL
+      this.control.attach( gltf.scene );
+      this.scene.add( this.control );
+
+      gltf.scene.scale.set(3, 3, 3)
+      this.orbit.update();
+
+      this.renderer.render(this.scene, this.camera);
+    }, undefined,
+    error => {
+      console.log(error);
+    });
+},
+
   methods: {
-    init: function() {
-        let container = document.getElementById('container');
-        // SET AND ADD CAMERA
-        this.camera = new THREE.PerspectiveCamera(75, container.clientWidth/container.clientHeight, 0.1, 1000);
-        this.camera.position.z = 1;
-
-        // ADD SCENE
-        this.scene = new THREE.Scene();
-        // ADD LIGHT TO SCENE
-        this.light = new THREE.AmbientLight(0xffffff, 5.3);
-        this.scene.add(this.light);
-        // RENDER IN PAGE
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
-        this.renderer.setClearColor('#dbdbdb');
-        this.renderer.setSize(container.clientWidth, container.clientHeight);
-        this.renderer.gammaFactor = 1.5;
-        this.renderer.gammaOutput = true;
-        container.appendChild(this.renderer.domElement);
-
-        // SET GRID HELPER
-        const size = 10;
-        const divisions = 25;
-        this.gridHelper = new THREE.GridHelper( size, divisions );
-        this.scene.add( this.gridHelper );
-
-        console.log("hello world");
-
-        // SET ORBIT CONTROLS
-        this.orbit = new OrbitControls( this.camera, this.renderer.domElement );
-        this.camera.position.set( 0, 2, 2 );
-        this.orbit.update();
-
-        this.orbit.addEventListener("change", () => this.renderer.render(this.scene, this.camera));
-
-        // SET MODEL CONTROL
-        this.control = new TransformControls( this.camera, this.renderer.domElement );
-        this.control.setSize(1);
-        this.control.addEventListener( 'change', () => { this.renderer.render(this.scene, this.camera); });
-
-
-        // EVENT LISTNER TO ENABLE ORBIT CAMERA AND CHANGE VALUE OF INPUTS
-        this.control.addEventListener( 'dragging-changed', ( event ) => {
-          const gltf = this.gltf[this.selectTool];
-          this[this.selectTool] = {x: gltf.x, y: gltf.y, z: gltf.z}
-          this.orbit.enabled = ! event.value});
-
-
-
-        // ADD GLTFLOADER
-        this.loader = new GLTFLoader();
-
-        this.loader.load("./meat.glb", gltf => {
-          this.gltf = gltf.scene;
-          this.scene.add(gltf.scene);
-
-          // ADD CONTROLS TO MODEL
-          this.control.attach( gltf.scene );
-    			this.scene.add( this.control );
-
-          gltf.scene.scale.set(3, 3, 3)
-          this.orbit.update();
-
-          this.renderer.render(this.scene, this.camera);
-        }, undefined,
-        error => {
-          console.log(error);
-        });
-
-    },
-
     // CONTROLS SET MODE CHANGE
-    posRotScale: function(e) {
+    positionRotationScale: function(e) {
+      console.log(this.$ref)
       switch (e.target.options.selectedIndex) {
 
         case 0:
@@ -150,16 +139,13 @@ export default {
 
     // INPUT CHAGE
     inputChange: function(e) {
-      const selectOpt = e.target.name
-      const setNum = this[selectOpt];
-      this.gltf[selectOpt].set(setNum.x, setNum.y, setNum.z)
+    console.log(this.$ref)
+      const selectOption = e.target.name
+      const setNumber = this[selectOption];
+      this.gltf[selectOption].set(setNumber.x, setNumber.y, setNumber.z)
       this.renderer.render(this.scene, this.camera);
     }
   },
-  mounted() {
-     this.init();
-
-  }
 }
 </script>
 
@@ -172,7 +158,7 @@ export default {
   margin: 0px;
   padding: 0px;
 }
-.mainCont {
+.mainContainer {
   width: 500px;
   margin: auto;
 }
